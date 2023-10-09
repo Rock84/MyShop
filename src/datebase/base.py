352b2438c5998744285c6481cc18f1ea9d1372d5
Rock84 <pank_rock@tut.by>
2023-10-09 19:05:16 +0300

    base_settings

--- src/datebase/base.py
+++ src/datebase/base.py
@@ -6,7 +6,7 @@ from src.settings.settings import engine, get_session
 class Base(DeclarativeBase):
     id: Mapped[str] = mapped_column(primary_key=True)
     engine_base: Mapped[str] = mapped_column(engine)
-    session_base: Mapped[str] =mapped_column(get_session())
+    session_base: Mapped[str] = mapped_column(get_session())
 
     @declared_attr
     def __tablename__(cls) -> str:
